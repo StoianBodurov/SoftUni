01127e44d79471caa89e274608e6b522578b1ddf
StoianBodurov <stoian.bodurov@abv.bg>
2021-01-20 20:42:02 +0200

    _

--- Python/Advanced/03-Multidimensional-Lists/lab/4_symbol_in_matrix.py
+++ Python/Advanced/03-Multidimensional-Lists/lab/4_symbol_in_matrix.py
@@ -1,5 +1,4 @@
 rows = int(input())
-
 matrix = []
 
 for _ in range(rows):
@@ -9,6 +8,7 @@ for _ in range(rows):
 find_symbol = input()
 indexes = tuple()
 is_find = False
+
 for r in range(len(matrix)):
     for c in range(len(matrix[r])):
         if matrix[r][c] == find_symbol:
@@ -19,5 +19,6 @@ for r in range(len(matrix)):
 
     if is_find:
         break
+
 if not indexes:
     print(f"{find_symbol} does not occur in the matrix")
