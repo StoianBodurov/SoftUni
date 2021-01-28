9241dee3c62e2d77f1f32eda3d9da646c1242f7d
StoianBodurov <stoian.bodurov@abv.bg>
2021-01-28 15:41:50 +0200

    4.	Operate

---
+++ Python/Advanced/05-Functions Advanced/lab/4_operate.py
@@ -0,0 +1,15 @@
+from functools import reduce
+
+
+def operate(operator, *args):
+    operators = {
+        '+': lambda x, y: x + y,
+        '-': lambda x, y: x -y,
+        '*': lambda x, y: x * y,
+        '/': lambda x, y: x / y,
+    }
+    return reduce(operators[operator], args)
+
+
+print(operate("+", 1, 2, 3))
+print(operate("*", 3, 4))
