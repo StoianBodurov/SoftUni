1f11d6bc0bbb2835567caddfedea0070f4232e47
StoianBodurov <stoian.bodurov@abv.bg>
2021-02-08 14:19:12 +0200

    4. Fashion Boutique

--- Python/Advanced/01-Lists as Stacks and Queues/exercise/4_fashion_boutique.py
+++ Python/Advanced/01-Lists as Stacks and Queues/exercise/4_fashion_boutique.py
@@ -1,22 +1,17 @@
 clothes = [int(el) for el in input().split(' ')]
 rack_capacity = int(input())
-clothing_sum = 0
-rack_count = 0
+current_capacity = rack_capacity
+rack_count = 1
 
 while len(clothes) > 0:
     current_clothing = clothes.pop()
-    clothing_sum += current_clothing
-    if clothing_sum > rack_capacity:
-        clothing_sum = 0
+    if current_clothing <= current_capacity:
+        current_capacity -= current_clothing
+
+    else:
         rack_count += 1
-        clothes.append(current_clothing)
-    if clothing_sum == rack_capacity:
-        rack_count += 1
-        clothing_sum = 0
-    if not clothes:
-        rack_count += 1
+        current_capacity = rack_capacity
+        current_capacity -= current_clothing
+
 
 print(rack_count)
-
-
-
