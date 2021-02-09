7fb1ef68a103d894ac020ce958dfa413ff3feef4
StoianBodurov <stoian.bodurov@abv.bg>
2021-02-09 21:40:16 +0200

    Magic Triangle

---
+++ Python/Advanced/00-Exams/16_dec_2020/problem_3.py
@@ -0,0 +1,21 @@
+def get_magic_triangle(n):
+    matrix = []
+    for r in range(n):
+        row = []
+        if r == 0:
+            row.append(1)
+        else:
+            for c in range(len(matrix[r-1]) + 1):
+                col1 = 0
+                col2 = 0
+
+                if 0 <= c - 1 < len(matrix[r-1]):
+                    col1 = matrix[r-1][c - 1]
+                if 0 <= c < len(matrix[r-1]):
+                    col2 = matrix[r-1][c]
+                row.append(col1 + col2)
+        matrix.append(row)
+    return matrix
+
+
+print(get_magic_triangle(7))
