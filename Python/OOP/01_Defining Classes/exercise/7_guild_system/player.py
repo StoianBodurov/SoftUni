08dd1c776211f0ea3cdf0dd5224b44324d6b9bf1
StoianBodurov <stoian.bodurov@abv.bg>
2021-03-02 15:30:52 +0200

    7.Guild System

---
+++ Python/OOP/01_Defining Classes/exercise/7_guild_system/player.py
@@ -0,0 +1,19 @@
+class Player:
+    def __init__(self, name, hp, mp):
+        self.name = name
+        self.hp = hp
+        self.mp = mp
+        self.skills = {}
+        self.guild = 'Unaffiliated'
+
+    def add_skill(self, skill_name, mana_cost):
+        if skill_name in self.skills:
+            return 'Skill already added'
+        self.skills[skill_name] = mana_cost
+        return f'Skill {skill_name} added to the collection of the player {self.name}'
+
+    def player_info(self):
+        if self.skills:
+            player_skills = '\n'.join([f'==={k} - {v}'for k, v in self.skills.items()])
+            return f"Name: {self.name}\nGuild: {self.guild}\nHP: {self.hp}\nMP: {self.mp}\n{player_skills}\n"
+        return f"Name: {self.name}\nGuild: {self.guild}\nHP: {self.hp}\nMP: {self.mp}\n"
