c08d36bdd50a5d2ed585492bdbbef7d1c1ee52f7
StoianBodurov <stoian.bodurov@abv.bg>
2021-03-10 11:25:50 +0200

    Exercise: Encapsulation 2.Pizza Calories

---
+++ Python/OOP/04_encapsulation/exercise/02_pizza _calories/project/topping.py
@@ -0,0 +1,20 @@
+class Topping:
+    def __init__(self, topping_type, weight):
+        self.__topping_type = topping_type
+        self.__weight = weight
+
+    @property
+    def topping_type(self):
+        return self.__topping_type
+
+    @topping_type.setter
+    def topping_type(self, new_type):
+        self.__topping_type = new_type
+
+    @property
+    def weight(self):
+        return self.__weight
+
+    @weight.setter
+    def weight(self, new_weight):
+        self.__weight = new_weight
