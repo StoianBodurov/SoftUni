42782904e34b20fe101ea3ee8c1c8caafa25a044
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-18 15:37:41 +0200

    6. Strong Number

---
+++ C#/Fundamentals/Intro and Basic Syntax/exercise/6. Strong Number/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _6._Strong_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            int factorialSum = 0;
+
+            for (var i = 0; i < number.Length; i++)
+            {
+                int n = int.Parse(number[i].ToString());
+                Console.WriteLine(n);
+                int factorial = 1;
+                for (var j = 1; j <= n; j++)
+                {
+                    factorial *= j;
+                    //Console.WriteLine(j);
+                }
+                factorialSum += factorial;
+                //Console.WriteLine(factorial);
+                
+            }
+
+            if (int.Parse(number) == factorialSum)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
