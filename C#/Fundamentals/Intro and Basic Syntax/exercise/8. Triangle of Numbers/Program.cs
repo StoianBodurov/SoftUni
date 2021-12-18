93f7295fe7a1063952100a1c5ccdeea40837a109
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-18 15:58:59 +0200

    8. Triangle of Numbers

---
+++ C#/Fundamentals/Intro and Basic Syntax/exercise/8. Triangle of Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _8._Triangle_of_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (var i = 1; i <= n; i++)
+            {
+                for (var j = 0; j < i; j++)
+                {
+                    Console.Write($"{i} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
