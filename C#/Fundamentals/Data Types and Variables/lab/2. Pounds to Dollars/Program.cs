c0a55b8a02ad739baebe80acbae888f070df50d2
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-18 22:54:06 +0200

    2. Pounds to Dollars

---
+++ C#/Fundamentals/Data Types and Variables/lab/2. Pounds to Dollars/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _2._Pounds_to_Dollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal pounds = decimal.Parse(Console.ReadLine());
+            decimal poundToDolar = 1.31m;
+            decimal dolars = pounds * poundToDolar;
+            Console.WriteLine($"{dolars:f3}");
+
+        }
+    }
+}
