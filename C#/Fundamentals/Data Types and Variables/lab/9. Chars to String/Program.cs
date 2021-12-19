cf45ca41972e128436452ac0bf7607b660122fe3
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-19 21:30:29 +0200

    9. Chars to String

---
+++ C#/Fundamentals/Data Types and Variables/lab/9. Chars to String/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _9._Chars_to_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstSymbol = char.Parse(Console.ReadLine());
+            char secondSymbol = char.Parse(Console.ReadLine());
+            char thirdSymbol = char.Parse(Console.ReadLine());
+
+            string result = firstSymbol.ToString() + secondSymbol.ToString() + thirdSymbol.ToString();
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
