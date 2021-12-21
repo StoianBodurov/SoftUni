29ec3c850fe0dd8a4d3e1a11b3dea415493c1b8d
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-21 22:19:41 +0200

    2. Sum Digits

---
+++ C#/Fundamentals/Data Types and Variables/exercise/2. Sum Digits/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _2._Sum_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            while (number > 0)
+            {
+                int n = number % 10;
+                sum += n;
+                number /= 10;
+            }
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
