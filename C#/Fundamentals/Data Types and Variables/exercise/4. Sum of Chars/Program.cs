abfe7c6e5f4a8058b49293793c13961bdc9a2042
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-22 08:53:53 +0200

    4. Sum of Chars

---
+++ C#/Fundamentals/Data Types and Variables/exercise/4. Sum of Chars/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _4._Sum_of_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int charSum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                string letter = Console.ReadLine();
+                charSum += (int)letter[0];
+            }
+            Console.WriteLine($"The sum equals: {charSum}");
+        }
+    }
+}
