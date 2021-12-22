f80a76f3b151a0149675182dea7aad5f28fbdbb7
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-22 17:01:12 +0200

    4. Reverse Array of Strings

---
+++ C#/Fundamentals/Arrays/lab/4. Reverse Array of Strings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _4._Reverse_Array_of_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine().Split();
+
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                Console.Write(array[i] + ' ');
+            }
+        }
+    }
+}
