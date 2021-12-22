f67a259d337454c198b3f9516db9960b7220a5fc
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-22 12:02:48 +0200

    8. Beer Kegs

---
+++ C#/Fundamentals/Data Types and Variables/exercise/8. Beer Kegs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _8._Beer_Kegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double maxVolume = 0;
+            string maxVolumeModel = "";
+            
+
+            for (int i = 0; i < n; i++)
+            {
+                string modelVolum = Console.ReadLine();
+                double radius = float.Parse(Console.ReadLine()) ;
+                int heigt = int.Parse(Console.ReadLine());
+
+                double volume = Math.PI * heigt * radius * radius;
+                Console.WriteLine(volume);
+
+                if (volume > maxVolume)
+                {
+                    maxVolume = volume;
+                    maxVolumeModel = modelVolum;
+                  
+                }
+               
+            }
+
+            Console.WriteLine(maxVolumeModel);
+        }
+    }
+}
