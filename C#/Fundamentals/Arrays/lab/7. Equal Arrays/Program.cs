d2e44d89d6c88c30a7263980d49d16e2868a2ecb
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-22 17:55:12 +0200

    7. Equal Arrays

---
+++ C#/Fundamentals/Arrays/lab/7. Equal Arrays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _7._Equal_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] array2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int arraySum = 0;
+            bool isEqual = true;
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i] != array2[i])
+                {
+                    isEqual = false;
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    break;
+
+                }
+
+                arraySum += array1[i];
+            }
+
+            if (isEqual)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {arraySum}");
+            }
+
+        }
+    }
+}
