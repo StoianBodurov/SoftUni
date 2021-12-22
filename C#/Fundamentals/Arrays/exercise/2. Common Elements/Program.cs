66bb0db12606b117dc9666ed3d1714fe0f2d7ceb
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-22 19:52:32 +0200

    2. Common Elements

---
+++ C#/Fundamentals/Arrays/exercise/2. Common Elements/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _2._Common_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array1 = Console.ReadLine().Split();
+            string[] array2 = Console.ReadLine().Split();
+
+            foreach (var el in array2)
+            {
+                foreach (var e in array1)
+                {
+                    if (el == e)
+                    {
+                        Console.Write(e + ' ');
+                    }
+                }
+            }
+        }
+    }
+}
