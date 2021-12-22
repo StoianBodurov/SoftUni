7285b4b5869c46b57631e2af1c3beaa75490a85d
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-22 16:52:23 +0200

    3. Rounding Numbers

---
+++ C#/Fundamentals/Arrays/lab/3. Rounding Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Rounding_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            int[] roundedNumbers = new int[numbers.Length];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                roundedNumbers[i] = (int) Math.Round(numbers[i], MidpointRounding.AwayFromZero);
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"{numbers[i]} => {roundedNumbers[i]}");
+            }
+        }
+    }
+}
