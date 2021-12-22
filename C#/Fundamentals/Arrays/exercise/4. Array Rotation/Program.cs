6fc0a77c03eaa6aa6efd858559a73a02a9bf9b12
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-22 21:53:26 +0200

    4. Array Rotation

---
+++ C#/Fundamentals/Arrays/exercise/4. Array Rotation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _4._Array_Rotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int numberOfRotatin = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfRotatin; i ++)
+            {
+                int tempElement = numbers[0];
+                for (int j = 1; j < numbers.Length; j++)
+                {
+                    numbers[j - 1] = numbers[j];
+                }
+                numbers[numbers.Length - 1] = tempElement;
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
