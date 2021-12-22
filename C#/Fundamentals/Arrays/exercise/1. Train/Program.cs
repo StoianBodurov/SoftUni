e335cf5e15d839f62b30094fb7774a4ee988f7ec
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-22 19:51:48 +0200

    1. Train

---
+++ C#/Fundamentals/Arrays/exercise/1. Train/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _1._Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wagons = int.Parse(Console.ReadLine());
+            int[] people = new int[wagons];
+            int sum = 0;
+
+            for (int i = 0; i < wagons; i++)
+            {
+                int countOfPeople = int.Parse(Console.ReadLine());
+                sum += countOfPeople;
+                people[i] = countOfPeople;
+            }
+
+            Console.WriteLine(string.Join(' ', people));
+            Console.WriteLine(sum);
+        }
+    }
+}
