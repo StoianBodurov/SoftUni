c2274e778c3b18d8d753bf6c4a01e3db2db4ef17
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-22 09:05:15 +0200

    7. Water Overflow

---
+++ C#/Fundamentals/Data Types and Variables/exercise/7. Water Overflow/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _7._Water_Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int tankCapacity = 255;
+            int tankCurrentQuantiti = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int waterQuantiti = int.Parse(Console.ReadLine());
+                if ((tankCurrentQuantiti + waterQuantiti) > tankCapacity)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+                else
+                {
+                    tankCurrentQuantiti += waterQuantiti;
+                }
+            }
+            Console.WriteLine(tankCurrentQuantiti);
+        }
+    }
+}
