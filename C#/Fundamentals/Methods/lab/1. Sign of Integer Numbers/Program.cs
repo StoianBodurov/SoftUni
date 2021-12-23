7d97cf654ce46000df76107715918cd7e53b045f
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-23 21:58:06 +0200

    1. Sign of Integer Numbers

---
+++ C#/Fundamentals/Methods/lab/1. Sign of Integer Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _1._Sign_of_Integer_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            printSugn(n);
+        }
+
+        static void printSugn(int n)
+        {
+            if (n > 0)
+            {
+                Console.WriteLine($"The number {n} is positive.");
+            }
+            else if (n < 0)
+            {
+                Console.WriteLine($"The number {n} is negative.");
+            }
+            else
+            {
+                Console.WriteLine($"The number 0 is zero.");
+            }
+        }
+    }
+}
