f7b7d48f541f4f96c26b7f6721996684cfa1d0bc
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-23 22:10:04 +0200

    4. Printing Triangle

---
+++ C#/Fundamentals/Methods/lab/4. Printing Triangle/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _4._Printing_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            PrintingTriangle(n);
+        }
+
+        static void PrintingTriangle(int n)
+        {
+            for (int row = 1; row <= n; row++)
+            {
+                PrintRow(row);
+            }
+
+            for (int row = n - 1; row > 0; row--)
+            {
+                PrintRow(row);
+            }
+        }
+
+        static void PrintRow(int row)
+        {
+            for (int j = 1; j <= row; j++)
+            {
+                Console.Write($"{j} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
