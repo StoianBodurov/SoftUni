8ebaac85ae63b5b789b20434fb3787fd1b25210a
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-23 18:47:41 +0200

    8. Magic Sum

---
+++ C#/Fundamentals/Arrays/exercise/8. Magic Sum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _8._Magic_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int givenNumber = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (givenNumber == numbers[i] + numbers[j])
+                    {
+                        Console.WriteLine($"{numbers[i]} {numbers[j]}");
+                    }
+                }
+            }
+
+        }
+    }
+}
