8026e628e7778e14fe2aafde7053d775291fed2b
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-23 22:46:30 +0200

    8. Math Power

---
+++ C#/Fundamentals/Methods/lab/8. Math Power/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _8._Math_Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            int pow = int.Parse(Console.ReadLine());
+
+            double result =PowNumber(number, pow);
+            Console.WriteLine(result);
+
+        }
+
+        static double PowNumber(double number, int pow)
+        {
+            double result = 0d;
+            result = Math.Pow(number, pow);
+
+
+            return result;
+
+        }
+    }
+}
