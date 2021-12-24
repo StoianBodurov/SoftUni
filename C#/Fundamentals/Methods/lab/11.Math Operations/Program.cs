2a74b1555c872d6448057ffcaef25e8379038f3f
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-24 09:16:12 +0200

    11.Math Operations

---
+++ C#/Fundamentals/Methods/lab/11.Math Operations/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _11.Math_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            string operatot = Console.ReadLine();
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Calculate(num1, operatot, num2));
+        }
+
+        static double Calculate(int num1, string operatot, int num2)
+        {
+            switch (operatot)
+            {
+                case "+":
+                    return num1 + num2;
+                case "-":
+                    return num1 - num2;
+                case "*":
+                    return num1 * num2;
+                case "/":
+                    return num1 / num2;
+            }
+            return 0;
+        } 
+        
+    }
+}
