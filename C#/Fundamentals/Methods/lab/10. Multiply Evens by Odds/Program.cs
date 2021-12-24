0315faa924ba890337cf50d48b05fc1713e9c69d
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-24 09:08:18 +0200

    10. Multiply Evens by Odds

---
+++ C#/Fundamentals/Methods/lab/10. Multiply Evens by Odds/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _10._Multiply_Evens_by_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Math.Abs(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine(GetMultipleOfEvenAndOdds(number));
+        }
+
+        static int GetMultipleOfEvenAndOdds(int number)
+        {
+            return GetSumOfEvenDigits(number) * GetSumOfOddDigits(number);
+        }
+
+        static int GetSumOfOddDigits(int number)
+        {
+            int sum = 0;
+            while (number > 0)
+            {
+
+                int n = number % 10;
+                if (n % 2 == 0)
+                {
+                    sum += n;
+                }
+                number /= 10;
+            }
+            return sum;
+        }
+
+        private static int GetSumOfEvenDigits(int number)
+        {
+            int sum = 0;
+            while (number > 0)
+            {
+                int n = number % 10;
+                if (n % 2 != 0)
+                {
+                    sum += n;
+                }
+                number /= 10;
+            }
+            return sum;
+        }
+    }
+}
