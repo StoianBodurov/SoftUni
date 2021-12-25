f9a38d8a025855369d7b54811c41f5ce4f6ba49b
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-25 17:45:23 +0200

    3. Merging Lists\

---
+++ C#/Fundamentals/Lists/lab/3. Merging Lists/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Merging_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> firstList = Console.ReadLine().Split().ToList();
+            List<string> secondtList = Console.ReadLine().Split().ToList();
+            List<string> result = new List<string>();
+ 
+
+            if (secondtList.Count() > firstList.Count())
+            {
+                for (int i = 0; i < secondtList.Count(); i++)
+                {
+                    if (firstList.Count() > i)
+                    {
+                        result.Add(firstList[i]);
+                    }
+                    result.Add(secondtList[i]);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < firstList.Count(); i++)
+                {
+                    result.Add(firstList[i]);
+                    if (secondtList.Count() > i)
+                    {
+                        result.Add(secondtList[i]);
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(' ', result));
+
+
+        }
+    }
+}
