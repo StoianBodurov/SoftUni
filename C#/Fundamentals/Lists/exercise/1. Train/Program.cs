e31f2cb75b77476d3c1ab4d4fefd8715807a8610
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-25 22:39:16 +0200

    1. Train

---
+++ C#/Fundamentals/Lists/exercise/1. Train/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagons = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int wagonCapacity = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                List<string> commands = Console.ReadLine().Split().ToList();
+                
+                if (commands[0] == "end")
+                {
+                    break;
+                }
+
+                if (commands[0] == "Add")
+                {
+                    int passengers = int.Parse(commands[1]);
+                    wagons.Add(passengers);
+                }
+                else
+                {
+                    int passengers = int.Parse(commands[0]);
+                    AddPassengers(wagons, passengers, wagonCapacity);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", wagons));
+        }
+
+        static void AddPassengers(List<int> wagons, int passengers, int wagonCapacity)
+        {
+            for (int i = 0; i < wagons.Count(); i++)
+            {
+                if (wagonCapacity - wagons[i] >= passengers)
+                {
+                    wagons[i] += passengers;
+                    break;
+                }
+            }
+        }
+    }
+}
