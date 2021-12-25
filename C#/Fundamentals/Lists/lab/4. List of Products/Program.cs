27610846610c56b11331b7ece7ebdd38ba6a9508
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-25 17:54:16 +0200

    4. List of Products

---
+++ C#/Fundamentals/Lists/lab/4. List of Products/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4._List_of_Products
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> products = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string product = Console.ReadLine();
+                products.Add(product);
+            }
+
+            products.Sort();
+
+            for (int i = 1; i <= products.Count; i++)
+            {
+                Console.WriteLine($"{i}.{products[i - 1]}");
+            }
+        }
+    }
+}
