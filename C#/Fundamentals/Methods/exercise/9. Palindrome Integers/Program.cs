d29733b3a1c768ba7652934af74fcc3afa71f77b
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-25 16:10:56 +0200

    9. Palindrome Integers

---
+++ C#/Fundamentals/Methods/exercise/9. Palindrome Integers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _9._Palindrome_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "END")
+                {
+                    break;
+                }
+                if (CheckIsPalindrom(input))
+                {
+                    Console.WriteLine("true") ;
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+            }
+        }
+
+        static bool CheckIsPalindrom(string input)
+        {
+
+            for (int i = 0; i < input.Length / 2; i++)
+            {
+                if (input[i] != input[input.Length - 1 - i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
