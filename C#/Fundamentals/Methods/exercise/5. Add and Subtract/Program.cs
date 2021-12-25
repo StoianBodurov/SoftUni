544250783e81c2cffb83569b6b4042c7e91a0491
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-25 14:55:04 +0200

    5. Add and Subtract

---
+++ C#/Fundamentals/Methods/exercise/5. Add and Subtract/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _5._Add_and_Subtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int z = int.Parse(Console.ReadLine());
+
+            int result = Subtract(Add(x, y), z);
+
+            Console.WriteLine(result);
+        }
+
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        static int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
