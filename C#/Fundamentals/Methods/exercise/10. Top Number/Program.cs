bd9988768a0c3ad624126064d5156116e015e373
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-25 16:12:24 +0200

    10. Top Number

---
+++ C#/Fundamentals/Methods/exercise/10. Top Number/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _10._Top_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (PrintMasterNumber(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        static bool PrintMasterNumber(int number)
+        {
+            bool isMasterNumber = false;
+            bool isOdd = false;
+            int digitSum = 0;
+            while (number > 0)
+            {
+                int n = number % 10;
+                if (n % 2 != 0)
+                {
+                    isOdd = true;
+                }
+                digitSum += n;
+                number /= 10;
+            }
+        
+            if  (digitSum % 8 == 0 && isOdd)
+            {
+                isMasterNumber = true;
+            }
+            
+
+            return isMasterNumber;
+        }
+    }
+}
