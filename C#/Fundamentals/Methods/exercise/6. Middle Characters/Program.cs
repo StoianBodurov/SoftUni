a6dba790ed6786bd9f0ed66835aa93c1e816be57
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-25 15:10:25 +0200

    6. Middle Characters

---
+++ C#/Fundamentals/Methods/exercise/6. Middle Characters/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _5._Middle_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            Console.WriteLine(GetMiddleCharacter(text));
+        }
+
+        static string GetMiddleCharacter(string text)
+        {
+            string result = "";
+            int MiddleIndex = text.Length / 2;
+            if (text.Length % 2 == 0)
+            {
+                result = text[MiddleIndex - 1].ToString() + text[MiddleIndex].ToString();
+            }
+            else
+            {
+                
+                result = text[MiddleIndex].ToString();
+            }
+            return result;
+        }
+    }
+}
