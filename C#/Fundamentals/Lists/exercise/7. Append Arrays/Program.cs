f3ee76782d9c90530df91c5cc85458a426690130
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-26 12:32:21 +0200

    7. Append Arrays

---
+++ C#/Fundamentals/Lists/exercise/7. Append Arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7._Append_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrays = Console.ReadLine().Split("|");
+            List<string> result = new List<string>();
+
+            for (int i = arrays.Length - 1; i >= 0; i--)
+            {
+                string[] array = arrays[i].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                foreach(var el in array)
+                {
+                    result.Add(el);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
