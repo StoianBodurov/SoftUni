1436595732bd723bf5c010fece4d1f99d4ff9103
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-30 09:10:59 +0200

    1. Randomize Words

---
+++ C#/Fundamentals/Objects and Classes/lab/1. Randomize Words/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Randomize_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = Console.ReadLine().Split().ToList();
+
+            Random rnd = new Random();
+            int minValue = 0;
+            int maxValue = words.Count;
+
+            while( words.Count > 0)
+            {
+                
+                string randomWord =  words[rnd.Next(minValue, maxValue)];
+                Console.WriteLine(randomWord);
+                words.Remove(randomWord);
+                maxValue--;
+            }
+            
+
+        }
+    }
+}
