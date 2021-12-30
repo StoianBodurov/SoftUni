2e68f4dd552deab3b1bee057b2beb70cf5ecd42c
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-30 10:25:30 +0200

    5. Students 2.0

---
+++ C#/Fundamentals/Objects and Classes/lab/5. Students 2.0/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._Students_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            while (true)
+            {
+                string[] data = Console.ReadLine().Split();
+
+                if (data[0] == "end")
+                {
+                    break;
+                }
+
+                string firstName = data[0];
+                string lastName = data[1];
+                int age = int.Parse(data[2]);
+                string hometown = data[3];
+
+                if (!IsStudentExisting(students, firstName, lastName))
+                {
+                    Student student = new Student
+                    {
+                        FisrtName = firstName,
+                        LastName = lastName,
+                        Age = age,
+                        City = hometown,
+                    };
+                    students.Add(student);
+                }
+
+                else
+                {
+                    Student currentStudent = GetStudent(students, firstName, lastName);
+                    currentStudent.Age = age;
+                    currentStudent.City = hometown;
+                }
+            }
+
+            string cityName = Console.ReadLine();
+
+            List<Student> filterdStudents = students
+                .Where(s => s.City == cityName)
+                .ToList();
+
+            foreach (var student in filterdStudents)
+            {
+                Console.WriteLine($"{student.FisrtName} {student.LastName} is {student.Age} years old.");
+            }
+        }
+
+        static Student GetStudent(List<Student> students, string firstName, string lastName)
+        {
+            Student existingStudent = null;
+            foreach (var student in students)
+            {
+                if (student.FisrtName == firstName && student.LastName == lastName)
+                {
+                    existingStudent = student;
+                }
+            }
+
+            return existingStudent;
+        }
+
+        static bool IsStudentExisting(List<Student> students, string firstName, string lastName)
+        {
+            foreach (var student in students)
+            {
+                if (student.FisrtName == firstName && student.LastName == lastName)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ C#/Fundamentals/Objects and Classes/lab/5. Students 2.0/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._Students_2._0
+{
+    class Student
+    {
+        public string FisrtName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+    }
+}
