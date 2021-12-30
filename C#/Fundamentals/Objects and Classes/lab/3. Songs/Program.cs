17c84b4c0f6068b8f32aefe255bf53364e2fd877
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-30 09:39:50 +0200

    3. Songs

---
+++ C#/Fundamentals/Objects and Classes/lab/3. Songs/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3._Songs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            List<Song> songs = new List<Song>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] data = Console.ReadLine().Split("_");
+
+                Song song = new Song();
+
+                song.TypeList = data[0];
+                song.Name = data[1];
+                song.Time = data[2];
+                songs.Add(song);
+            }
+
+            string typeList = Console.ReadLine();
+
+            if (typeList == "all")
+            {
+                foreach(var song in songs)
+                {
+                    Console.WriteLine(song.Name);
+                }
+            }
+            else
+            {
+                foreach (var song in songs)
+                {
+                    if (typeList == song.TypeList)
+                    {
+                        Console.WriteLine(song.Name);
+                    }
+                }
+            }
+            
+        }
+    }
+
+}
