20026e8e2f8ee68e3e87eeeb7bc2d0abf82cf8ce
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-30 09:56:01 +0200

    4. Students

---
+++ C#/Fundamentals/Objects and Classes/lab/4. Students/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4._Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Student> students = new List<Student>();
+            while (true)
+            {
+                string[] data = Console.ReadLine().Split();
+                if (data[0] == "end")
+                {
+                    break;
+                }
+
+                string firstName = data[0];
+                string lastName = data[1];
+                int age = int.Parse(data[2]);
+                string hometown = data[3];
+
+                Student studen = new Student
+                {
+                    FisrtName = firstName,
+                    LastName = lastName,
+                    Age = age,
+                    City = hometown,
+                };
+
+
+                students.Add(studen);
+
+
+            }
+
+            string cityName = Console.ReadLine();
+
+            List<Student> filterdStudents = students
+                .Where(s => s.City == cityName)
+                .ToList();
+
+            foreach (var student in filterdStudents)
+            {
+                Console.WriteLine($"{student.FisrtName} {student.LastName} is {student.Age} years old.");
+            }
+        }
+    }
+}
---
+++ C#/Fundamentals/Objects and Classes/lab/4. Students/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._Students
+{
+    class Student
+    {
+        public string FisrtName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+    }
+}
