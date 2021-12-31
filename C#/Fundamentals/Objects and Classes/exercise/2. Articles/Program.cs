0c98df28556cbbea04e8f5708d3e0de697c64e84
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-31 18:52:54 +0200

    2. Articles

---
+++ C#/Fundamentals/Objects and Classes/exercise/2. Articles/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _2._Articles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split(", ");
+            string title = data[0];
+            string content = data[1];
+            string author = data[2];
+
+            Article article = new Article(title, content, author);
+            
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] token = Console.ReadLine().Split(": ");
+
+                string command = token[0];
+                string newContent = token[1];
+
+                if (command == "Edit")
+                {
+                    article.Edit(newContent);
+                }
+                else if (command == "ChangeAuthor")
+                {
+                    article.ChangeAuthor(newContent);
+                }
+                else
+                {
+                    article.Rename(newContent);
+                }
+
+            }
+
+            Console.WriteLine($"{article.Title} - {article.Content}: {article.Author}");
+
+        }
+    }
+}
