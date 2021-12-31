71a97ca4cf4f45c2cbdc332b88c4eec0dfe53bec
StoianBodurov <stoian.bodurov@abv.bg>
2021-12-31 19:59:09 +0200

    3. Articles 2.0

---
+++ C#/Fundamentals/Objects and Classes/exercise/3. Articles 2.0/Article.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._Articles_2._0
+{
+    class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+
+        }
+
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public void Edit(string newContent)
+        {
+            this.Content = newContent;
+        }
+
+        public void ChangeAuthor(string newAuthor)
+        {
+            this.Author = newAuthor;
+        }
+
+        public void Rename(string newTitle)
+        {
+            this.Title = newTitle;
+        }   
+    }
+}
---
+++ C#/Fundamentals/Objects and Classes/exercise/3. Articles 2.0/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3._Articles_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Article> articles = new List<Article>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] data = Console.ReadLine().Split(", ");
+                string title = data[0];
+                string content = data[1];
+                string autor = data[2];
+
+                articles.Add(new Article(title, content, autor));
+
+            }
+
+            string criteria = Console.ReadLine();
+
+            if (criteria == "title")
+            {
+                articles.Sort((x, y) => x.Title.CompareTo(y.Title));
+            }
+            else if (criteria == "content")
+            {
+                articles.Sort((x, y) => x.Content.CompareTo(y.Content));
+            }
+            else
+            {
+                articles.Sort((x, y) => x.Author.CompareTo(y.Author));
+
+            }
+
+            foreach (Article article in articles)
+            {
+                Console.WriteLine($"{article.Title} - {article.Content}: {article.Author}");
+            }
+        }
+    }
+}
