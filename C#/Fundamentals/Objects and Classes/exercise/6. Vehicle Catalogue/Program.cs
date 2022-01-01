6efb0a438ee7189b55ab3e2d0e8e5748e5a74cbc
StoianBodurov <stoian.bodurov@abv.bg>
2022-01-01 18:18:01 +0200

    6. Vehicle Catalogue

---
+++ C#/Fundamentals/Objects and Classes/exercise/6. Vehicle Catalogue/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _6._Vehicle_Catalogue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Vehicle> vehicles = new List<Vehicle>();
+
+            while (true)
+            {
+                string[] data = Console.ReadLine().Split();
+
+                if (data[0] == "End")
+                {
+                    break;
+                }
+
+                string type = data[0];
+                type = type[0].ToString().ToUpper() + type.Substring(1);
+                string model = data[1];
+                string color = data[2];
+                int horsePower = int.Parse(data[3]);
+
+                vehicles.Add(new Vehicle(type, model, color, horsePower));
+
+            }
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "Close the Catalogue")
+                {
+                    break;
+                }
+
+                Vehicle serched = vehicles.Find(v => command == v.Model);
+
+                if (serched != null)
+                {
+                    Console.WriteLine($"Type: {serched.Type}");
+                    Console.WriteLine($"Model: {serched.Model}");
+                    Console.WriteLine($"Color: {serched.Color}");
+                    Console.WriteLine($"Horsepower: {serched.HorsePower}");
+
+                }
+            }
+
+            List<Vehicle> cars = vehicles.FindAll(v => v.Type == "Car");
+            List<Vehicle> trucks = vehicles.FindAll(v => v.Type == "Truck");
+
+            Console.WriteLine($"Cars have average horsepower of: {AverageHorspower(cars):f2}.");
+            Console.WriteLine($"Trucks have average horsepower of: {AverageHorspower(trucks)}.");
+        }
+
+        static double AverageHorspower(List<Vehicle> vehicles)
+        {
+            if (vehicles.Count == 0)
+            {
+                return 0;
+            }
+            double sumHorspower = 0;
+
+            foreach (var v in vehicles)
+            {
+                sumHorspower += v.HorsePower;
+            }
+
+            return sumHorspower / vehicles.Count();
+            
+        }
+    }
+}
---
+++ C#/Fundamentals/Objects and Classes/exercise/6. Vehicle Catalogue/Vehicle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._Vehicle_Catalogue
+{
+    class Vehicle
+    {
+
+        public Vehicle (string type, string model, string color, int horsePower)
+        {
+            Type = type;
+            Model = model;
+            Color = color;
+            HorsePower = horsePower;
+        }
+        public string Type { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int HorsePower { get; set; }
+    }
+}
