14ac211912bc0d3ad23ea8f8a384c59545f3d846
StoianBodurov <stoian.bodurov@abv.bg>
2022-01-01 17:29:38 +0200

    7. Order by Age

---
+++ C#/Fundamentals/Objects and Classes/exercise/7. Order by Age/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._Order_by_Age
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ C#/Fundamentals/Objects and Classes/exercise/7. Order by Age/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7._Order_by_Age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> persons = new List<Person>();
+
+            while (true)
+            {
+                string[] data = Console.ReadLine().Split();
+
+                if (data[0] == "End")
+                {
+                    break;
+                }
+
+                string name = data[0];
+                string id = data[1];
+                int age = int.Parse(data[2]);
+
+                Person person = new Person
+                {
+                    Name = name,
+                    Id = id,
+                    Age = age,
+                };
+
+                persons.Add(person);
+            }
+
+            persons.Sort((x, y) => x.Age.CompareTo(y.Age));
+
+            foreach (Person p in persons)
+            {
+                Console.WriteLine($"{p.Name} with ID: {p.Id} is {p.Age} years old.");
+            }
+        }
+    }
+}
