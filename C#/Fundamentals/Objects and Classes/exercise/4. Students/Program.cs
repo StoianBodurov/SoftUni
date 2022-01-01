f9785f67e02b737ed92133b06904804b719b669d
StoianBodurov <stoian.bodurov@abv.bg>
2022-01-01 17:16:26 +0200

    4. Students

---
+++ C#/Fundamentals/Objects and Classes/exercise/4. Students/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4._Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Student> students = new List<Student>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] data = Console.ReadLine().Split();
+                string firstName = data[0];
+                string lastName = data[1];
+                double grade = double.Parse(data[2]);
+
+                Student student = new Student
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Grade = grade,
+                };
+                students.Add(student);
+            }
+
+            students.Sort((x, y) => y.Grade.CompareTo(x.Grade));
+
+            foreach (Student student in students)
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName}: {student.Grade:f2}");
+            }
+        }
+    }
+}
---
+++ C#/Fundamentals/Objects and Classes/exercise/4. Students/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._Students
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Grade { get; set; }
+
+    }
+}
