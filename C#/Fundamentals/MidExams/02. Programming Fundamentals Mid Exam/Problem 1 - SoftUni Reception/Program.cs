ce19ccb8e824b4ef1d4a0b4c683d1adacc0ff853
StoianBodurov <stoian.bodurov@abv.bg>
2022-01-03 23:24:13 +0200

    Problem 1 - SoftUni Reception

---
+++ C#/Fundamentals/MidExams/02. Programming Fundamentals Mid Exam/Problem 1 - SoftUni Reception/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Problem_1___SoftUni_Reception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fullEfficiency = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                int efficiency = int.Parse(Console.ReadLine());
+                fullEfficiency += efficiency;
+            }
+            int studentsCount = int.Parse(Console.ReadLine());
+
+            int hours = 0;
+            int breakcount = 1;
+            while (true)
+            {
+                if (studentsCount <= 0)
+                {
+                    break;
+                }
+
+                if (breakcount % 4 == 0)
+                {
+                    breakcount = 1;
+                    hours++;
+                    continue;
+                }
+                studentsCount -= fullEfficiency;
+                hours++;
+                breakcount++;
+            }
+            
+
+            Console.WriteLine($"Time needed: {hours}h.");
+        }
+    }
+}
