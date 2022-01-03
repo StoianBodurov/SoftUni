f46d1d9c3eeb35c7711fc537658128a854451dd1
StoianBodurov <stoian.bodurov@abv.bg>
2022-01-03 21:12:16 +0200

    Problem 1 - Computer Store

---
+++ C#/Fundamentals/MidExams/01. Programming Fundamentals Mid Exam Retake/Problem 1 - Computer Store/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Problem_1___Computer_Store
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double totalPrice = 0;
+            string command = Console.ReadLine();
+            while (true)
+            {
+
+                if (command == "special" || command == "regular")
+                {
+                    break;
+                }
+
+                double price = double.Parse(command);
+                if (price < 0)
+                {
+                    Console.WriteLine("Invalid price!");
+                    
+                }
+                else
+                {
+                    totalPrice += price;
+
+                }
+                command = Console.ReadLine();
+            }
+
+            if (totalPrice == 0)
+            {
+                Console.WriteLine("Invalid order!");
+            }
+            else
+            {
+                double taxes = totalPrice * 0.2;
+                double priceWitTaxes = totalPrice * 1.2;
+
+                if (command == "special")
+                {
+                    priceWitTaxes *= 0.9;
+                }
+                Console.WriteLine("Congratulations you've just bought a new computer!");
+                Console.WriteLine($"Price without taxes: {totalPrice:f2}$");
+                Console.WriteLine($"Taxes: {taxes:f2}$");
+                Console.WriteLine("-----------");
+                Console.WriteLine($"Total price: {priceWitTaxes:f2}$");
+            }
+        }
+    }
+}
