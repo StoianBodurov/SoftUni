7d7d4894ae3cdf7cb62c93459f96afb67e9f3333
StoianBodurov <stoian.bodurov@abv.bg>
2022-01-04 22:53:37 +0200

    1. Count Real Numbers

---
+++ C#/Fundamentals/Associative Arrays/lab/1. Count Real Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Count_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            SortedDictionary<int, int> countDict = new SortedDictionary<int, int>();
+
+            foreach (var num in numbers)
+            {
+                if (!countDict.ContainsKey(num))
+                {
+                    countDict.Add(num, 0); 
+                }
+
+                countDict[num] += 1;
+            }
+
+            foreach (var item in countDict)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
