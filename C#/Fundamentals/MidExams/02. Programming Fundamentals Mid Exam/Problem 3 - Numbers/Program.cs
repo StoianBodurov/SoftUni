0f4760813fa0f11a48a0416ae82db4dc616f805a
StoianBodurov <stoian.bodurov@abv.bg>
2022-01-04 08:50:00 +0200

    Problem 3 - Numbers

---
+++ C#/Fundamentals/MidExams/02. Programming Fundamentals Mid Exam/Problem 3 - Numbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_3___Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] data = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            List<int> result = new List<int>();
+
+            double average = data.Sum() / data.Length;
+            
+            foreach (var el in data)
+            {
+                if (el > average)
+                {
+                    result.Add((int)el);
+                }
+            }
+
+            result.Sort((x, y) => y.CompareTo(x));
+            if (result.Count > 0)
+            {
+                if (result.Count > 5)
+                {
+                    result = result.GetRange(0, 5);
+                }
+                Console.WriteLine(string.Join(" ", result));
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+            
+        }
+    }
+}
