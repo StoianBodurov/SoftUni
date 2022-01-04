c13e3ef2808153653d55b5cccd935d53b6c2bf7c
StoianBodurov <stoian.bodurov@abv.bg>
2022-01-04 23:06:08 +0200

    2. Odd Occurrences

---
+++ C#/Fundamentals/Associative Arrays/lab/2. Odd Occurrences/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._Odd_Occurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split().Select(w => w.ToLower()).ToArray();
+
+            Dictionary<string, int> wordCounter = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                if (!wordCounter.ContainsKey(word))
+                {
+                    wordCounter.Add(word, 0);
+                }
+                wordCounter[word]++;
+            }
+
+            foreach (var item in wordCounter)
+            {
+                if (item.Value % 2 != 0)
+                {
+                    Console.Write(item.Key + " ");
+                }
+            }
+
+        }
+    }
+}
