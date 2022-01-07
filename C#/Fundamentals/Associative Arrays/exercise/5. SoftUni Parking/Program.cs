dc312b1e98d6995bd573d9560f781d63ace0f59f
StoianBodurov <stoian.bodurov@abv.bg>
2022-01-07 16:51:59 +0200

    5. SoftUni Parking

---
+++ C#/Fundamentals/Associative Arrays/exercise/5. SoftUni Parking/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5._SoftUni_Parking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, string> parking = new Dictionary<string, string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] token = Console.ReadLine().Split();
+
+                string command = token[0];
+                string userName = token[1];
+
+                if (command == "register")
+                {
+                    if (parking.ContainsKey(userName))
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {parking[userName]}");
+                    }
+                    else
+                    {
+                        string plateNumber = token[2];
+                        parking[userName] = plateNumber;
+                        Console.WriteLine($"{userName} registered {plateNumber} successfully");
+                    }
+                }
+                else
+                {
+                    if (!parking.ContainsKey(userName))
+                    {
+                        Console.WriteLine($"ERROR: user {userName} not found");
+                    }
+                    else
+                    {
+                        parking.Remove(userName);
+                        Console.WriteLine($"{userName} unregistered successfully");
+                    }
+
+                }
+            }
+
+            foreach (var item in parking)
+            {
+                Console.WriteLine($"{item.Key} => {item.Value}");
+            }
+        }
+    }
+}
