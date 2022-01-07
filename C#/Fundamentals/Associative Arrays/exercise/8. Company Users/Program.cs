deb9ab5564c08adbb8e0316a183c8a7090773a91
StoianBodurov <stoian.bodurov@abv.bg>
2022-01-07 18:18:09 +0200

    8. Company Users

---
+++ C#/Fundamentals/Associative Arrays/exercise/8. Company Users/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _8._Company_Users
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> companies = new Dictionary<string, List<string>>();
+
+            while (true)
+            {
+                string[] token = Console.ReadLine().Split(" -> ");
+
+                if (token[0] == "End")
+                {
+                    break;
+                }
+
+                string company = token[0];
+                string employId = token[1];
+
+                if (!companies.ContainsKey(company))
+                {
+                    companies.Add(company, new List<string>());
+                }
+
+                if (!companies[company].Contains(employId))
+                {
+                    companies[company].Add(employId);
+
+                }
+            }
+
+            Dictionary<string, List<string>> sortedCompanies = companies.OrderBy(c => c.Key).ToDictionary(c => c.Key, c => c.Value);
+
+            foreach (var item in sortedCompanies)
+            {
+                Console.WriteLine(item.Key);
+
+                foreach (var id in item.Value)
+                {
+                    Console.WriteLine($"-- {id}");
+                }
+            }
+        }
+    }
+}
