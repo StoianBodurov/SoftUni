8f31dd1e7ded7e3aadfc327f8319620a8bf85965
StoianBodurov <stoian.bodurov@abv.bg>
2022-01-07 08:37:39 +0200

    3. Word Synonyms

---
+++ C#/Fundamentals/Associative Arrays/lab/3. Word Synonyms/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3._Word_Synonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<string>> words = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+                if (!words.ContainsKey(word))
+                {
+                    words.Add(word, new List<string>());
+                }
+
+                words[word].Add(synonym);
+            }
+
+            foreach (var item in words)
+            {
+                Console.WriteLine($"{item.Key} - {String.Join(", ",item.Value)}");
+            }
+        }
+    }
+}
