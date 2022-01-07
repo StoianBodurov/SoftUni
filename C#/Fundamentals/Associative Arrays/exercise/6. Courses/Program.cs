c8bb48627eda4903e8c2c607f1d0cb5b88362934
StoianBodurov <stoian.bodurov@abv.bg>
2022-01-07 17:09:50 +0200

    6. Courses

---
+++ C#/Fundamentals/Associative Arrays/exercise/6. Courses/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _6._Courses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();
+
+            while (true)
+            {
+                string[] token = Console.ReadLine().Split(" : ");
+
+                if (token[0] == "end")
+                {
+                    break;
+                }
+
+                string coursName = token[0];
+                string studentName = token[1];
+
+                if (!courses.ContainsKey(coursName))
+                {
+                    courses.Add(coursName, new List<string>());
+                }
+
+                courses[coursName].Add(studentName);
+            }
+
+            Dictionary<string, List<string>> sortedCourses = courses.OrderByDescending(c => c.Value.Count).ToDictionary(c => c.Key, c => c.Value);
+            foreach(var item in sortedCourses)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value.Count}");
+                item.Value.Sort((x, y) => x.CompareTo(y));
+
+                foreach(string student in item.Value)
+                {
+                    Console.WriteLine($"-- {student}");
+                }
+            }
+        }
+    }
+}
