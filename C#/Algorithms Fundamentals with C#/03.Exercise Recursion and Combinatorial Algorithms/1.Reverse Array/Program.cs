a174651c3bda93b0fcd36d93a76f904114dbb68e
StoianBodurov <stoian.bodurov@abv.bg>
2022-01-17 10:59:34 +0200

    1.Reverse Array

---
+++ C#/Algorithms Fundamentals with C#/03.Exercise Recursion and Combinatorial Algorithms/1.Reverse Array/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _0._3Recursion_and_Combinatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            reversAray(numbers, numbers.Length - 1);
+        }
+
+        private static void reversAray(int[] numbers, int index)
+        {
+            if (index < 0)
+            {
+                return;
+            }
+
+            Console.Write(numbers[index] + " ");
+            reversAray(numbers, index - 1);
+            
+        }
+    }
+}
