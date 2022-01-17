1185cb676eb29f209bfcc3d3fa5048e2e1fed48a
StoianBodurov <stoian.bodurov@abv.bg>
2022-01-17 11:24:23 +0200

    2. Nested Loops To Recursion

---
+++ C#/Algorithms Fundamentals with C#/03.Exercise Recursion and Combinatorial Algorithms/2. Nested Loops To Recursion/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _2._Nested_Loops_To_Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] result = new int[n];
+
+
+            genResult(0, result);
+        }
+
+        private static void genResult(int index, int[] result)
+        {
+            if (index >= result.Length)
+            {
+                Console.WriteLine(string.Join(" ", result));
+                return;
+            }
+
+            for (int i = 1; i <= result.Length; i++)
+            {
+                result[index] = i;
+                genResult(index + 1, result);
+            }
+        }
+    }
+}
