db63845ad405c132126a2e6c909aab218c65ca44
StoianBodurov <stoian.bodurov@abv.bg>
2022-01-20 17:56:10 +0200

    4. Variations with Repetition

---
+++ C#/Algorithms Fundamentals with C#/02.Combinatorial Problems/lab/4. Variations with Repetition/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _4._Variations_with_Repetition
+{
+    class Program
+    {
+        private static string[] elements;
+        private static string[] variation;
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split();
+            int slotsNumber = int.Parse(Console.ReadLine());
+            variation = new string[slotsNumber];
+
+            GetVariations(0);
+        }
+
+        private static void GetVariations(int index)
+        {
+            if (index >= variation.Length)
+            {
+                Console.WriteLine(string.Join(" ", variation));
+            }
+            else
+            {
+                for (int i = 0; i < elements.Length; i++)
+                {
+                    variation[index] = elements[i];
+                    GetVariations(index + 1);
+                }
+            }
+        }
+    }
+}
