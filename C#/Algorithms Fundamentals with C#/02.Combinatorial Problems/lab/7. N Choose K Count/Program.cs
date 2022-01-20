678c219073bf59239757ec97d5fe3c969fa7445f
StoianBodurov <stoian.bodurov@abv.bg>
2022-01-20 18:01:05 +0200

    7. N Choose K Count

---
+++ C#/Algorithms Fundamentals with C#/02.Combinatorial Problems/lab/7. N Choose K Count/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _7._N_Choose_K_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetChoose(n, k));
+        }
+
+        private static long GetChoose(int n, int k)
+        {
+            if (n <= 1 || k == 0 || k == n)
+            {
+                return 1;
+            }
+            return GetChoose(n - 1, k) + GetChoose(n - 1, k - 1);
+        }
+    }
+}
