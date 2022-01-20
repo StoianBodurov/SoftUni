d34cc2ba5e9fe6e93fa0c7a0773e96eba389a19a
StoianBodurov <stoian.bodurov@abv.bg>
2022-01-20 22:11:23 +0200

    1. Binary Search

---
+++ C#/Algorithms Fundamentals with C#/04.Searching, Sorting and Greedy Algorithms/lab/1. Binary Search/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Binary_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int searchNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(BinarySearch(numbers, searchNumber));
+        }
+
+        private static int BinarySearch(int[] numbers, int searchNumber)
+        {
+            int start = 0;
+            int end = numbers.Length - 1;
+
+            while (start <= end)
+            {
+
+                int mid = (end + start) / 2;
+
+                if (numbers[mid] == searchNumber)
+                {
+                    return mid;
+                }
+
+                if (numbers[mid] < searchNumber)
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid - 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
