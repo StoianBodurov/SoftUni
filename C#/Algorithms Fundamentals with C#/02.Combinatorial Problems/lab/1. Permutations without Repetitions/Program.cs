1a170813d774b9a80ee7fb9e4f5624a9c8e0e93e
StoianBodurov <stoian.bodurov@abv.bg>
2022-01-20 17:36:43 +0200

    1. Permutations without Repetitions

---
+++ C#/Algorithms Fundamentals with C#/02.Combinatorial Problems/lab/1. Permutations without Repetitions/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _1._Permutations_without_Repetitions
+{
+    class Program
+    {
+        private static string[] elements;
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split();
+
+            Permute(0);
+        }
+
+        private static void Permute(int index)
+        {
+            if (index >= elements.Length)
+            {
+                Console.WriteLine(string.Join(" ", elements));
+            }
+            else
+            {
+                Permute(index + 1);
+                for (int i = index + 1; i < elements.Length; i++)
+                {
+                    Swap(index, i);
+                    Permute(index + 1);
+                    Swap(index, i);
+                }
+            }
+        }
+
+        private static void Swap(int index, int i)
+        {
+            var temp = elements[index];
+            elements[index] = elements[i];
+            elements[i] = temp;
+        }
+    }
+}
