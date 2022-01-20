d7e63f9f0ad7b160021f8a0cb4e97bd4669dad21
StoianBodurov <stoian.bodurov@abv.bg>
2022-01-20 22:30:29 +0200

    3. Bubble Sort

---
+++ C#/Algorithms Fundamentals with C#/04.Searching, Sorting and Greedy Algorithms/lab/3. Bubble Sort/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Bubble_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = 1; j < numbers.Length - i; j++)
+                {
+                    if (numbers[j - 1] > numbers[j])
+                    {
+                        Swap(numbers, j - 1, j);
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        private static void Swap(int[] numbers, int i, int j)
+        {
+            int temp = numbers[i];
+            numbers[i] = numbers[j];
+            numbers[j] = temp;
+        }
+    }
+}
