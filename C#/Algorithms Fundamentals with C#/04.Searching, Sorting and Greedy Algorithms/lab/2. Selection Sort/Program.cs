c68fb72de228a284f735bcc0f5fbd37bb353b9c9
StoianBodurov <stoian.bodurov@abv.bg>
2022-01-20 22:21:17 +0200

    2. Selection Sort

---
+++ C#/Algorithms Fundamentals with C#/04.Searching, Sorting and Greedy Algorithms/lab/2. Selection Sort/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._Selection_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int index = 0; index < numbers.Length; index++)
+            {
+                int min = index;
+                for (int current = index + 1; current < numbers.Length; current++)
+                {
+                    if (numbers[current] < numbers[min])
+                    {
+                        min = current;
+                    }
+                }
+
+                Swap(numbers, index, min);
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        private static void Swap(int[] numbers, int index, int min)
+        {
+            int temp = numbers[min];
+            numbers[min] = numbers[index];
+            numbers[index] = temp;
+        }
+    }
+}
