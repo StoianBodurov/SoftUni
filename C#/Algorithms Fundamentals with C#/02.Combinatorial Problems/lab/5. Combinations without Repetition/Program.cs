320ef2985675956f92bebd43c351eff997109d88
StoianBodurov <stoian.bodurov@abv.bg>
2022-01-20 17:57:59 +0200

    5. Combinations without Repetition

---
+++ C#/Algorithms Fundamentals with C#/02.Combinatorial Problems/lab/5. Combinations without Repetition/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _5._Combinations_without_Repetition
+{
+    class Program
+    {
+        private static string[] elements;
+        private static string[] slots;
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split();
+            int n = int.Parse(Console.ReadLine());
+            slots = new string[n];
+
+            GetCombinations(0, 0);
+        }
+
+        private static void GetCombinations(int index, int start)
+        {
+            if (index >= slots.Length)
+            {
+                Console.WriteLine(string.Join(" ", slots));
+            }
+            else
+            {
+                for (int i = start; i < elements.Length; i++)
+                {
+                    slots[index] = elements[i];
+                    GetCombinations(index + 1, i + 1);
+                }
+            }
+        }
+    }
+}
