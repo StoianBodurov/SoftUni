5b408281f7dbaa9301d07748c40f30d1fd404f91
StoianBodurov <stoian.bodurov@abv.bg>
2022-01-22 17:21:06 +0200

    1. Reverse Strings

---
+++ C#/Fundamentals/Text Processing/lab/1. Reverse Strings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _1._Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                string text = Console.ReadLine();
+
+                if (text == "end")
+                {
+                    break;
+                }
+                string result = "";
+
+                for (int i = text.Length - 1; i >= 0; i--)
+                {
+                    result += text[i];
+                }
+                Console.WriteLine($"{text} = {result}");
+            }
+            
+        }
+    }
+}
