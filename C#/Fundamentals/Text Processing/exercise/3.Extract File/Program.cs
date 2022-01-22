eabf88fa69e3b74811b91217a54255848ac75a10
StoianBodurov <stoian.bodurov@abv.bg>
2022-01-22 21:20:36 +0200

    3.Extract File

---
+++ C#/Fundamentals/Text Processing/exercise/3.Extract File/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _3.Extract_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] filePath = Console.ReadLine().Split("\\");
+
+            string[] token = filePath[filePath.Length - 1].Split(".");
+            string fileName = token[0];
+            string fileExtension = token[1];
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {fileExtension}");
+
+        }
+    }
+}
