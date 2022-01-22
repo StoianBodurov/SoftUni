c05d746922c91f038c6a98c7a741ac60e5084cf3
StoianBodurov <stoian.bodurov@abv.bg>
2022-01-22 21:41:19 +0200

    6.Replace Repeating Chars

---
+++ C#/Fundamentals/Text Processing/exercise/6.Replace Repeating Chars/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace _6.Replace_Repeating_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            StringBuilder result = new StringBuilder();
+            result.Append(text[0]);
+
+            for (int i = 1; i < text.Length; i++)
+            {
+                if (text[i -1] != text[i])
+                {
+                    result.Append(text[i]);
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
