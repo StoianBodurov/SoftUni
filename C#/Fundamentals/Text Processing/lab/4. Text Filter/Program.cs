45f98c55fc3750a1cea5eb22b0c5498caf56806e
StoianBodurov <stoian.bodurov@abv.bg>
2022-01-22 17:45:40 +0200

    4. Text Filter

---
+++ C#/Fundamentals/Text Processing/lab/4. Text Filter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _4._Text_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] banWords = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+
+            foreach (string word in banWords)
+            {
+                text = text.Replace(word, new String('*', word.Length));
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
