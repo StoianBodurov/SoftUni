3d762ac2f9229d81a0e8408e3d0b19f288a89487
StoianBodurov <stoian.bodurov@abv.bg>
2022-01-22 21:36:13 +0200

    4.Caesar Cipher

---
+++ C#/Fundamentals/Text Processing/exercise/4.Caesar Cipher/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace Caesar_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                int asciiNum = text[i];
+                char newchar = Convert.ToChar(asciiNum + 3);
+                result.Append(newchar);
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
