c3dbad3dc1c52cd2e76f89f361c3297c204e6206
StoianBodurov <stoian.bodurov@abv.bg>
2022-01-22 18:28:50 +0200

    2.  Character Multiplier

---
+++ C#/Fundamentals/Text Processing/exercise/2.  Character Multiplier/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _2.__Character_Multiplier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split();
+            string firstString = data[0];
+            string secondString = data[1];
+            int result = 0;
+
+            for (int i = 0; i < Math.Max(firstString.Length, secondString.Length); i++)
+            {
+                int firstCode = 1;
+                int secondCode = 1;
+
+                if (firstString.Length > i)
+                {
+                    firstCode = firstString[i];
+                }
+                if (secondString.Length > i)
+                {
+                    secondCode = secondString[i];
+                }
+
+                result += firstCode * secondCode; 
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
