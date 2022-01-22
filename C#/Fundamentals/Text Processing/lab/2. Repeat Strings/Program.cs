ff2d36bf3bacbd0dd914ff4c15bea4016e023be3
StoianBodurov <stoian.bodurov@abv.bg>
2022-01-22 17:30:39 +0200

    2. Repeat Strings

---
+++ C#/Fundamentals/Text Processing/lab/2. Repeat Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace _2._Repeat_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split();
+
+            StringBuilder result = new StringBuilder();
+
+            foreach (var word in data)
+            {
+                for (int i = 0; i < word.Length; i++)
+                    result.Append(word);
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
