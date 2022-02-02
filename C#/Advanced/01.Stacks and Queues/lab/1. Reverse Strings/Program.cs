a0fa8ed7d8f16913745d0ff4d3d8cc89b507734d
StoianBodurov <stoian.bodurov@abv.bg>
2022-02-02 09:50:23 +0200

    1. Reverse Strings

---
+++ C#/Advanced/01.Stacks and Queues/lab/1. Reverse Strings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _1._Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = Console.ReadLine();
+            Stack<char> result = new Stack<char>();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                result.Push(data[i]);
+            }
+
+            while (result.Count > 0)
+            {
+                Console.Write(result.Pop());
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
