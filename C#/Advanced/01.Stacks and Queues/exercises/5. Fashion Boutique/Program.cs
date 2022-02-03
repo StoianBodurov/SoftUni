53d4b184f806d2a33e4cb1f796384c277469e2d9
StoianBodurov <stoian.bodurov@abv.bg>
2022-02-03 14:57:54 +0200

    5. Fashion Boutique

---
+++ C#/Advanced/01.Stacks and Queues/exercises/5. Fashion Boutique/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._Fashion_Boutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> clothes = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            int boxCapacity = int.Parse(Console.ReadLine());
+            int capacityLeft = boxCapacity;
+            int boxCount = 1;
+
+            while (clothes.Count > 0)
+            {
+                
+                int cloths = clothes.Pop();
+                if (cloths <= capacityLeft)
+                {
+                    capacityLeft -= cloths;
+                }
+                else
+                {
+                    capacityLeft = boxCapacity - cloths;
+                    boxCount++;
+                }
+            }
+
+            Console.WriteLine(boxCount);
+
+        }
+    }
+}
