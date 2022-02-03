757180c90d791f567f134414c4e3f177fcdd8a12
StoianBodurov <stoian.bodurov@abv.bg>
2022-02-03 14:40:34 +0200

    4. Fast Food

---
+++ C#/Advanced/01.Stacks and Queues/exercises/4. Fast Food/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4._Fast_Food
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int food = int.Parse(Console.ReadLine());
+            int[] orders = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> queue = new Queue<int>(orders);
+
+            Console.WriteLine(queue.Max());
+
+            while (true)
+            {
+                if (queue.Count == 0)
+                {
+                    break;
+                }
+                int order = queue.Peek();
+                if (order <= food)
+                {
+                    food -= order;
+                    queue.Dequeue();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (queue.Count > 0)
+            {
+                Console.WriteLine($"Orders left: {string.Join(" ", queue)}");
+            }
+            else
+            {
+                Console.WriteLine("Orders complete");
+            }
+
+        }
+    }
+}
