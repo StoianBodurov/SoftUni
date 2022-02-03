e88d6f3309630027dd1cf0f09f6f5d132bbc5f80
StoianBodurov <stoian.bodurov@abv.bg>
2022-02-03 12:39:49 +0200

    2. Basic Queue Operations

---
+++ C#/Advanced/01.Stacks and Queues/exercises/2. Basic Queue Operations/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._Basic_Queue_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] commands = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int countElementsToAdd = commands[0];
+            int countElementsToRemoved = commands[1];
+            int searchedElement = commands[2];
+            Queue<int> queue = new Queue<int>();
+
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < countElementsToAdd; i++)
+            {
+                queue.Enqueue(numbers[i]);
+            }
+            for (int i = 0; i < countElementsToRemoved; i++)
+            {
+                if (queue.Count > 0)
+                {
+                    queue.Dequeue();
+
+                }
+            }
+
+            if (queue.Count > 0)
+            {
+                if (queue.Contains(searchedElement))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine(queue.Min());
+                }
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+
+
+        }
+    }
+}
