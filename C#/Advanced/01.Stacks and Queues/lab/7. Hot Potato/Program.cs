9d179a34a1ac498695029c878e72a5951441e70e
StoianBodurov <stoian.bodurov@abv.bg>
2022-02-03 10:17:38 +0200

    7. Hot Potato

---
+++ C#/Advanced/01.Stacks and Queues/lab/7. Hot Potato/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7._Hot_Potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] persons = Console.ReadLine().Split();
+            Queue<string> queue = new Queue<string>(persons);
+            int n = int.Parse(Console.ReadLine());
+            int count = 1;
+
+            while (queue.Count > 1)
+            {
+                if (count >= n)
+                {
+                    Console.WriteLine($"Removed {queue.Dequeue()}");
+                    count = 1;
+                }
+                else
+                {
+
+                    queue.Enqueue(queue.Dequeue());
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+
+        }
+    }
+}
