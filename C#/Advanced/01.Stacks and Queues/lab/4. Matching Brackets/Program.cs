d5a5071938bc1c234909c0b92a0a0ee3194601e7
StoianBodurov <stoian.bodurov@abv.bg>
2022-02-03 09:41:26 +0200

    4. Matching Brackets

---
+++ C#/Advanced/01.Stacks and Queues/lab/4. Matching Brackets/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _2._Matching_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = Console.ReadLine();
+            Stack<int> indexses = new Stack<int>();
+
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i] == '(')
+                {
+                    indexses.Push(i);
+                }
+                else if (data[i] == ')')
+                {
+                    int index = indexses.Pop();
+                    Console.WriteLine(data.Substring(index, i - index + 1)) ;
+
+                }
+            }
+        }
+    }
+}
