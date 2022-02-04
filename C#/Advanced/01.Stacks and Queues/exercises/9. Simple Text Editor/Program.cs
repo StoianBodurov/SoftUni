95711968e1b5c952cd443e2af4e8ab2566fcfd9c
StoianBodurov <stoian.bodurov@abv.bg>
2022-02-04 12:01:41 +0200

    9. Simple Text Editor

---
+++ C#/Advanced/01.Stacks and Queues/exercises/9. Simple Text Editor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _9._Simple_Text_Editor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nuberOfOperations = int.Parse(Console.ReadLine());
+            Stack<string> memeory = new Stack<string>();
+            string text = string.Empty;
+
+            for (int i = 0; i < nuberOfOperations; i++)
+            {
+                string[] token = Console.ReadLine().Split();
+                int command = int.Parse(token[0]);
+
+                switch (command)
+                {
+                    case 1:
+                        string newText = token[1];
+                        memeory.Push(text);
+                        text += newText;
+                        break;
+                    case 2:
+                        int countToErase = int.Parse(token[1]);
+                        memeory.Push(text);
+                        text = text.Substring(0, text.Length - countToErase);
+                        break;
+                    case 3:
+                        int index = int.Parse(token[1]) - 1;
+                        Console.WriteLine(text[index]);
+                        break;
+                    case 4:
+                        text = memeory.Pop();
+                        break;
+                }
+
+            }
+        }
+    }
+}
