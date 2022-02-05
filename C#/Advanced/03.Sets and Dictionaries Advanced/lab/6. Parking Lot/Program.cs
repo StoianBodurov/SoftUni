05c602ccd3dd5c37fa18e01831dda09db0c59e47
StoianBodurov <stoian.bodurov@abv.bg>
2022-02-05 17:15:28 +0200

    6. Parking Lot

---
+++ C#/Advanced/03.Sets and Dictionaries Advanced/lab/6. Parking Lot/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6._Parking_Lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> parkingLot = new HashSet<string>();
+            while (true)
+            {
+                string[] data = Console.ReadLine().Split(", ");
+                string command = data[0];
+
+                if (command == "END")
+                {
+                    break;
+                }
+
+                string carNumber = data[1];
+
+                if (command == "IN")
+                {
+                    parkingLot.Add(carNumber);
+                }
+                else if (command == "OUT")
+                {
+                    if (parkingLot.Contains(carNumber))
+                    {
+                        parkingLot.Remove(carNumber);
+                    }
+                }
+            }
+
+            if (parkingLot.Count > 0)
+            {
+                foreach(var el in parkingLot)
+                {
+                    Console.WriteLine(el);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
