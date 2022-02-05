cbea93e47256e064706a14104dbe8f5b3b75a474
StoianBodurov <stoian.bodurov@abv.bg>
2022-02-05 20:51:01 +0200

    3. Periodic Table

---
+++ C#/Advanced/03.Sets and Dictionaries Advanced/exercise/3. Periodic Table/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem_3._Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            SortedSet<string> elements = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] data = Console.ReadLine().Split();
+
+                foreach(var el in data)
+                {
+                    elements.Add(el);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", elements));
+        }
+    }
+}
