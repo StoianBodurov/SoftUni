96b5496768d17610c555f89c4bcf40a9a5259b1b
StoianBodurov <stoian.bodurov@abv.bg>
2022-02-05 17:35:34 +0200

    7. SoftUni Party

---
+++ C#/Advanced/03.Sets and Dictionaries Advanced/lab/7. SoftUni Party/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7._SoftUni_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> vip = new HashSet<string>();
+            HashSet<string> regular = new HashSet<string>();
+
+            while (true)
+            {
+                string student = Console.ReadLine();
+                if (student == "PARTY")
+                {
+                    break;
+                }
+
+                if (char.IsDigit(student[0]))
+                {
+                    vip.Add(student);
+                }
+                else
+                {
+                    regular.Add(student);
+                }
+            }
+
+            while (true)
+            {
+                string student = Console.ReadLine();
+                if (student == "END")
+                {
+                    break;
+                }
+
+                if (vip.Contains(student))
+                {
+                    vip.Remove(student);
+                }
+
+                if (regular.Contains(student))
+                {
+                    regular.Remove(student);
+                }
+            }
+
+            Console.WriteLine(vip.Count + regular.Count);
+            
+
+            
+
+            foreach(var el in vip)
+            {
+                Console.WriteLine(el);
+            }
+
+            foreach(var el in regular)
+            {
+                Console.WriteLine(el);
+            }
+        }
+    }
+}
