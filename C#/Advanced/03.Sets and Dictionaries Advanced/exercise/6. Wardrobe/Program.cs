654a1c0f8f7e1630905163696300cc7d335ed6ea
StoianBodurov <stoian.bodurov@abv.bg>
2022-02-06 12:12:24 +0200

    6. Wardrobe

---
+++ C#/Advanced/03.Sets and Dictionaries Advanced/exercise/6. Wardrobe/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6._Wardrobe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Dictionary<string, int>> wardrobe = new Dictionary<string, Dictionary<string, int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] data = Console.ReadLine().Split(" -> ");
+                string color = data[0];
+                string[] clothes = data[1].Split(",");
+
+                if (!wardrobe.ContainsKey(color))
+                {
+                    wardrobe.Add(color, new Dictionary<string, int>());
+                }
+                foreach(string clothe in clothes)
+                {
+                    if (!wardrobe[color].ContainsKey(clothe))
+                    {
+                        wardrobe[color].Add(clothe, 0);
+                    }
+                    wardrobe[color][clothe] += 1;
+                }
+            }
+            string[] searchCriteria = Console.ReadLine().Split();
+            string searchColor = searchCriteria[0];
+            string searchClothing = searchCriteria[1];
+
+            foreach (var keyValue in wardrobe)
+            {
+                bool isColor = false;
+                if (keyValue.Key == searchColor)
+                {
+                    isColor = true;
+                }
+                
+                Console.WriteLine($"{keyValue.Key} clothes");
+                foreach(var kv in keyValue.Value)
+                {
+                    if (isColor && kv.Key == searchClothing)
+                    {
+                        Console.WriteLine($" * {kv.Key} - {kv.Value} (found!)");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine($" * {kv.Key} - {kv.Value}");
+                    }
+                }
+            }
+        }
+    }
+}
