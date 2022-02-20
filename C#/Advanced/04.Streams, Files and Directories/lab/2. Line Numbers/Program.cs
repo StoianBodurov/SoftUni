81cd8e464d4924076e7c70f0c3774e04d009526d
StoianBodurov <stoian.bodurov@abv.bg>
2022-02-20 22:30:30 +0200

    2. Line Numbers

---
+++ C#/Advanced/04.Streams, Files and Directories/lab/2. Line Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace _2._Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader("../../../Input.txt"))
+            {
+                var line = reader.ReadLine();
+                int counter = 1;
+
+                while (line != null)
+                {
+                    Console.WriteLine($"{counter}. {line}");
+                    counter++;
+                    line = reader.ReadLine();
+                }
+            }
+        }
+    }
+}
