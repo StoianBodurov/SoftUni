496db590de6b35076b9bb43188ce48783bcccc4b
StoianBodurov <stoian.bodurov@abv.bg>
2022-02-20 22:24:12 +0200

    1. Odd Lines

---
+++ C#/Advanced/04.Streams, Files and Directories/lab/1. Odd Lines/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace _1._Odd_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader("../../../Input.txt"))
+            {
+                int counter = 0;
+                var line = reader.ReadLine();
+                while(line != null)
+                {
+                    if (counter % 2 != 0)
+                    {
+                        Console.WriteLine(line);
+                    }
+
+                    counter++;
+                    line = reader.ReadLine();
+                }
+            }
+        }
+    }
+}
