0fab150d900b7f77b4802dd6c61b096d6adca382
StoianBodurov <stoian.bodurov@abv.bg>
2022-02-24 08:56:55 +0200

    3. Count Uppercase Words

---
+++ C#/Advanced/05.Functional Programming/lab/3. Count Uppercase Words/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Count_Uppercase_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> GetUppercaseWords = x => char.IsUpper(x[0]);
+            string[] words = Console.ReadLine()
+                .Split( " ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(GetUppercaseWords).ToArray();
+
+            foreach(string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+       
+    }
+}
