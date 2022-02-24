41a118c3f437cc0ace7ebf9d1cf17ff4514cc1e5
StoianBodurov <stoian.bodurov@abv.bg>
2022-02-24 09:05:34 +0200

    4. Add VAT

---
+++ C#/Advanced/05.Functional Programming/lab/4. Add VAT/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _4._Add_VAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] prices = Console.ReadLine().Split(", ").Select(double.Parse).Select(x => x * 1.2).ToArray();
+
+            foreach(double price in prices)
+            {
+                Console.WriteLine($"{price:F2}");
+            }
+        }
+    }
+}
