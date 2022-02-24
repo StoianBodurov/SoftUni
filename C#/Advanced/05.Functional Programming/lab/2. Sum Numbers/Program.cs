b242866460a4b4dc59f8c0e9aaeae844439f4718
StoianBodurov <stoian.bodurov@abv.bg>
2022-02-24 08:41:41 +0200

    2. Sum Numbers

---
+++ C#/Advanced/05.Functional Programming/lab/2. Sum Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int numbersSum = GetSum(numbers);
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbersSum);
+        }
+
+        private static int GetSum(int[] numbers)
+        {
+            int sum = 0;
+            foreach(int number in numbers)
+            {
+                sum += number;
+            }
+
+            return sum;
+        }
+    }
+}
