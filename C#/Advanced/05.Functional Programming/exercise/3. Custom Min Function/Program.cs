af52981a9d0b18c4e5c798f0dcd2d5cdb67f9ae7
StoianBodurov <stoian.bodurov@abv.bg>
2022-02-25 07:07:42 +0200

    3. Custom Min Function

---
+++ C#/Advanced/05.Functional Programming/exercise/3. Custom Min Function/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            Func<int[], int> getMinNumber = numbers => numbers.Min();
+
+            Console.WriteLine(getMinNumber(numbers));
+        }
+    }
+}
