e5dc996784670011f73fe24a55e97afc06f0e93e
StoianBodurov <stoian.bodurov@abv.bg>
2022-02-25 08:24:45 +0200

    1. Action Point

---
+++ C#/Advanced/05.Functional Programming/exercise/1. Action Point/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _1._Action_Point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split(" ");
+
+            Action<string> print = name => Console.WriteLine(name);
+
+            foreach(string name in data)
+            {
+                print(name);
+            }
+        }
+    }
+}
