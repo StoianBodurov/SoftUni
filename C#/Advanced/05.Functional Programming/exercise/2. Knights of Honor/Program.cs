534683eb78f087ff882d97a7b707d66cb4648b27
StoianBodurov <stoian.bodurov@abv.bg>
2022-02-25 08:28:52 +0200

    2. Knights of Honor

---
+++ C#/Advanced/05.Functional Programming/exercise/2. Knights of Honor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _2._Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split(" ");
+            Action<string> print = name => Console.WriteLine($"Sir {name}");
+
+            foreach(string name in names)
+            {
+                print(name);
+            }
+        }
+    }
+}
