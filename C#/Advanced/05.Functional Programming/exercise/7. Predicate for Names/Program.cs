7941486ffe9e98140a6281ad25fb6bd20a2e2c8a
StoianBodurov <stoian.bodurov@abv.bg>
2022-02-28 14:03:51 +0200

    7. Predicate for Names

---
+++ C#/Advanced/05.Functional Programming/exercise/7. Predicate for Names/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _7._Predicate_for_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wordLength = int.Parse(Console.ReadLine());
+            Predicate<string> cheker = n => n.Length <= wordLength;
+
+            string[] names = Console.ReadLine().Split(" ").Where(w => cheker(w)).ToArray();
+
+            Console.WriteLine(string.Join("\n", names));
+        }
+    }
+}
