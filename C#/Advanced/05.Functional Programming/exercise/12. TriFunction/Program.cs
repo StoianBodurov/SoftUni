48ae2eb7cc3d91842f86b63aea82ae63f8dd6486
StoianBodurov <stoian.bodurov@abv.bg>
2022-02-28 18:06:42 +0200

    12. TriFunction

---
+++ C#/Advanced/05.Functional Programming/exercise/12. TriFunction/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _12._TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Func<string, bool> getFirstName = name => FilterName(name, n);
+            string names = Console.ReadLine().Split(" ").Where(n => getFirstName(n)).FirstOrDefault();
+            Console.WriteLine(names);
+            
+        }
+
+        public static bool FilterName(string name, int sum)
+        {
+            int charSum = 0;
+            for(int i = 0; i < name.Length; i++)
+            {
+                charSum += name[i];
+            }
+            if (charSum >= sum)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
