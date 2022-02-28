f9893be4b3d88cb0e87112eee08e46f32c8bfcb9
StoianBodurov <stoian.bodurov@abv.bg>
2022-02-28 13:58:18 +0200

    6. Reverse and Exclude

---
+++ C#/Advanced/05.Functional Programming/exercise/6. Reverse and Exclude/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _6._Reverse_and_Exclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = Console.ReadLine();
+            int number = int.Parse(Console.ReadLine());
+            Predicate<int> checker = n => GetNumbers(number, n);
+
+            int[] result = data.Split(" ").Select(int.Parse).Where(n => checker(n)).ToArray();
+
+            Console.WriteLine(string.Join(" ", result.Reverse()));
+        }
+
+        private static bool GetNumbers(int number, int n)
+        {
+            return n % number != 0;
+        }
+    }
+}
