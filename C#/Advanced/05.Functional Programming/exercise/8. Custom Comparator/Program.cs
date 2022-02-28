ebc6a83b89964e6315614553c8d331317614871b
StoianBodurov <stoian.bodurov@abv.bg>
2022-02-28 14:23:04 +0200

    8. Custom Comparator

---
+++ C#/Advanced/05.Functional Programming/exercise/8. Custom Comparator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _8._Custom_Comparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            Array.Sort(numbers, (x, y) => MyCompare(x, y));
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        private static int MyCompare(int x, int y)
+        {
+            if (x % 2 == 0 && y % 2 != 0)
+            {
+                return -1;
+            }
+
+            if(x % 2 != 0 && y % 2 == 0)
+            {
+                return 1;
+            }
+
+            return 0;
+
+
+        }
+    }
+}
