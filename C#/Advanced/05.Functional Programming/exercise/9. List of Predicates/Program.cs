31a2a3fc7f0d45e1aff9d4c0ac4b9789233dd900
StoianBodurov <stoian.bodurov@abv.bg>
2022-02-28 14:38:48 +0200

    8. Custom Comparator

---
+++ C#/Advanced/05.Functional Programming/exercise/9. List of Predicates/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _9._List_of_Predicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            Predicate<int> checker = n => IsDivide(n, numbers);
+
+            for(int i = 1; i <= number; i++)
+            {
+                if (checker(i))
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+        }
+
+        private static bool IsDivide(int number, int[] numbers)
+        {
+            bool divide = true;
+            foreach(var n in numbers)
+            {
+                if(number % n != 0)
+                {
+                    divide = false;
+                }
+            }
+
+            return divide;
+        }
+
+    }
+}
