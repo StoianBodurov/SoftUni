34868beee86934e4815750c38c4465e8076a3e47
StoianBodurov <stoian.bodurov@abv.bg>
2022-03-04 17:41:56 +0200

    2. Car Extension

---
+++ C#/Advanced/06.Defining classes/lab/2. Car Extension/Car.cs
@@ -0,0 +1,32 @@
+﻿namespace CarManufacturer
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+
+
+        public void Drive(double distance)
+        {
+            double requiredFuel = FuelConsumption * distance;
+
+            if (requiredFuel <= FuelQuantity)
+            {
+                FuelQuantity -= requiredFuel;
+            }
+            else
+            {
+                System.Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make} Model: { this.Model} Year: { this.Year} Fuel: { this.FuelQuantity:F2}";
+        }
+    }
+}
