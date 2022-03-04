f089f37f9fc14a1108a36e514fc3f171905d80dc
StoianBodurov <stoian.bodurov@abv.bg>
2022-03-04 23:15:01 +0200

    3. Car Constructors

---
+++ C#/Advanced/06.Defining classes/lab/3. Car Constructors/Car.cs
@@ -0,0 +1,27 @@
+﻿namespace CarManufacturer
+{
+    public class Car
+    {
+        public string Make { get; set; } = "VW";
+        public string Model { get; set; } = "Golf";
+        public int Year { get; set; } = 2025;
+        public double FuelQuantity { get; set; } = 200;
+        public double FuelConsumption { get; set; } = 10;
+
+        public Car() { }
+
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption): this(make, model, year)
+        {
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+        }
+
+    }
+}
