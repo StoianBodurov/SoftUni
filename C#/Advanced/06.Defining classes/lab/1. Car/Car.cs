7e80e3d44b8ab834aacb2b8b890a0e8206822029
StoianBodurov <stoian.bodurov@abv.bg>
2022-03-04 17:05:46 +0200

    1. Car

---
+++ C#/Advanced/06.Defining classes/lab/1. Car/Car.cs
@@ -0,0 +1,10 @@
+﻿
+namespace CarManufacturer
+{
+    public class Car
+    {
+        public int Year { get; set; }
+        public string Model { get; set; }
+        public string Make { get; set; }
+    }
+}
