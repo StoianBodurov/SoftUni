225c0847f50ad1cddd64bdff40ab77ee717fa295
StoianBodurov <stoian.bodurov@abv.bg>
2022-09-27 19:00:46 +0300

    02.Basic CRUD_lab

---
+++ MySQL/02.Basic CRUD/lab.sql
@@ -0,0 +1,37 @@
+USE `hotel`;
+
+# 1: Select Employee Information 
+SELECT `id`, `first_name`, `last_name`, `job_title` FROM `employees`
+ORDER BY `id`;
+
+# 2: Select Employees with Filter
+SELECT `id`, CONCAT(`first_name`, ' ', `last_name`) AS `full_name`, `job_title`, `salary` FROM `employees`
+WHERE `salary` > 1000
+ORDER BY `id`;
+
+# 3: Update Employees Salary
+UPDATE `employees`
+SET `salary` = `salary` + 100
+WHERE `job_title` = 'Manager';
+SELECT `salary` FROM `employees`;
+
+# 4: Top Paid Employee
+CREATE VIEW `top_five_of_employees` AS
+SELECT * FROM `employees`
+ORDER BY `salary` DESC
+LIMIT 1;
+
+SELECT * FROM `top_five_of_employees`;
+
+#  5: Select Employees by Multiple Filters
+SELECT * FROM `employees`
+WHERE `department_id` = 4 AND `salary` >= 100;
+
+# 6: Delete from Table
+DELETE FROM `employees`
+WHERE `department_id` IN(1, 2);
+
+SELECT * FROM `employees`
+ORDER BY `id`;
+
+
